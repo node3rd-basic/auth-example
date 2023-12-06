a810c31bec3b28c256b523b57f25e1c1a0fb4ab4
junhowoo <junho.woo@musinsa.com>
2023-12-06 20:45:26 +0900

    feat: 위변조 문자열 복호화 시도 예제 추가

--- example/crypt.js
+++ example/crypt.js
@@ -4,8 +4,18 @@ const secretKey = "secret key 123"
 // Encrypt
 const ciphertext = CryptoJS.AES.encrypt('1', secretKey).toString();
 console.log(ciphertext)
+
 // Decrypt
 const bytes  = CryptoJS.AES.decrypt(ciphertext, secretKey);
 const originalText = bytes.toString(CryptoJS.enc.Utf8);
 
 console.log(originalText);
+
+// 위변조 Decrypt
+const 위변조된_문자열 = "U2FsdGVkX1+AGcmuO7HcLuXV8Hm8+oYF15xWgk2Be+U="
+console.log(위변조된_문자열)
+
+const bytes2  = CryptoJS.AES.decrypt(위변조된_문자열, secretKey);
+const originalText2 = bytes2.toString(CryptoJS.enc.Utf8);
+
+console.log("위변조 시도 복호화: ", originalText2);
