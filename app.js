87a8af85950aa932efeed6e3cd2d8e55560c8329
junhowoo <junho.woo@musinsa.com>
2024-06-02 15:10:25 +0900

    Session 을 통한 로그인

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const cors = require("cors")
+const randomHash = require('random-hash')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 3000
 
-app.use(cors())
+app.use(cors({credentials:  true, origin: "http://localhost:63343"}))
+app.use(cookieParser())
 app.use(express.json())
+app.use(cookieParser())
 
 const users = [
     {
@@ -21,6 +25,8 @@ const users = [
     }
 ]
 
+const 세션저장소 = {}
+
 app.post("/sign-in", (req, res) => {
     const { email, password } = req.body
     const user = users.find((user) => user.email === email && user.password === password)
@@ -28,17 +34,28 @@ app.post("/sign-in", (req, res) => {
         return res.status(401).json({msg: "사용자를 찾지 못했습니다."})
     }
 
-    res.json({
-        token: user.id
-    })
+    const sessionId = randomHash.generateHash({ length: 12 })
+    세션저장소[sessionId] = user
+    res.cookie("_SESSION_ID", sessionId,{secure: false})
+
+    res.json({msg: "로그인 성공"})
 })
 
 app.get("/users/me", (req, res) => {
-    res.json({
-        id: 1,
-        name: "우준호",
-        email : "noggong@gmail.com",
-    })
+    console.log("세션저장소", 세션저장소)
+    const sessionId = req.cookies._SESSION_ID
+    const user = 세션저장소[sessionId]
+    if (!sessionId || !user) {
+        res.status(401).json({msg: "로그인이 필요합니다."})
+    }
+
+    res.json(user)
+})
+
+app.post("/sign-out", (req, res) => {
+    const sessionId = req.cookies._SESSION_ID
+    delete 세션저장소[sessionId]
+    res.json({msg: "로그아웃 성공"})
 })
 
 app.listen(port, () => {
